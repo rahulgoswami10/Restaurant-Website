87a64a47f903b7cddae73dd314ad7c99c30997dd
Rahul Goswami <therahulgoswami99@gmail.com>
2022-12-30 20:11:28 +0530

    updated

--- index.js
+++ index.js
@@ -32,6 +32,22 @@ $('.menu .list .btn').click(function(){
 })
 
 
+$(window).on('scroll load', function(){
+
+    $('section').each(function(){
+        let top = $(window).scrollTop();
+        let offset = $(this).offset().top - 200;
+        let height = $(this).height();
+        let id = $(this).attr('id');
+
+        if(top > offset && top < offset + height){
+            $('.navbar ul li a').removeClass('active');
+            $('.navbar').find(`[href="#${id}"]`).addClass('active');
+        }
+    })
+})
+
+
 // Theme toggle
 
 const off = document.querySelector('#off');
